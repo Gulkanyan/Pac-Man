9ec343195ef97e1b575373c5c753b753450e8b0b
Gulkanyan <gayane.gulkanyan@mail.ru>
2021-04-15 16:45:31 +0400

    Checking the last push

--- Core/StandardBlock.cpp
+++ Core/StandardBlock.cpp
@@ -8,7 +8,7 @@ StandardBlock::StandardBlock(QGraphicsItem *parent): QObject(), QGraphicsRectIte
     QPixmap pixmapItem(":/images/Images/Rectangle.png");
     pixmapItem = pixmapItem.scaled(QSize(50,50),  Qt::KeepAspectRatio);
     setBrush(QBrush(pixmapItem));
-
+// say hi
 }
 
 void StandardBlock::SetPosition(int x, int y)
