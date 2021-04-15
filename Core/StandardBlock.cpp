5699cccdfb2fbd76f3822e92db20e424df00e80d
Gulkanyan <gayane.gulkanyan@mail.ru>
2021-04-15 16:34:16 +0400

    level design changes

--- Core/StandardBlock.cpp
+++ Core/StandardBlock.cpp
@@ -5,8 +5,8 @@
 StandardBlock::StandardBlock(QGraphicsItem *parent): QObject(), QGraphicsRectItem(parent)
 {
     setRect(0, 0, 50, 50);
-    QPixmap pixmapItem(":/images/Images/Right.png");
-    pixmapItem = pixmapItem.scaled(QSize(48,48),  Qt::KeepAspectRatio);
+    QPixmap pixmapItem(":/images/Images/Rectangle.png");
+    pixmapItem = pixmapItem.scaled(QSize(50,50),  Qt::KeepAspectRatio);
     setBrush(QBrush(pixmapItem));
 
 }
